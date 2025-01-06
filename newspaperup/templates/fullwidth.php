41525515c832633b29b0a87726200750f82d84ef
Sean Murphy <sean@iamseanmurphy.com>
2025-01-06 03:19:25 +0000

    1.1.1

--- newspaperup/templates/fullwidth.php
+++ newspaperup/templates/fullwidth.php
@@ -12,17 +12,19 @@
 get_header(); ?>
 <main id="content" class="fullwidth-class content">
   <div class="container">
+    <!--==================== breadcrumb section ====================-->
+    <?php do_action('newspaperup_action_archive_page_title'); ?>
     <div class="row">
-      <!--==================== breadcrumb section ====================-->
-      <?php do_action('newspaperup_action_archive_page_title'); ?>
-      <div class="col-lg-12 bs-card-box padding-20">
-        <?php while ( have_posts() ) : the_post();
+      <div class="col-md-12">
+        <div class="bs-card-box padding-20">
+          <?php while ( have_posts() ) : the_post();
             the_content(); 
             // If comments are open or we have at least one comment, load up the comment template.
             if ( comments_open() || get_comments_number() ) :
               comments_template();
           endif;
-        endwhile; // End of the loop. ?>
+          endwhile; // End of the loop. ?>
+        </div>
       </div>
     </div>
   </div>
